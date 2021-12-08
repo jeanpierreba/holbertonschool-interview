eab0750a59fa21462a989be9e1ef3e7aae952f7d
jeanpierreba <2713@holbertonschool.com>
2021-12-08 17:15:30 -0500

    Fixed typo error

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -16,8 +16,8 @@ def canUnlockAll(boxes):
 
     for i in keys:
         for j in boxes[i]:
-            if j not in keys and j != i and j < len(boxes) and j!= 0:
-                keys.apend(j)
+            if j not in keys and j != i and j < len(boxes) and j != 0:
+                keys.append(j)
     if len(keys) == len(boxes):
         return True
     return False
