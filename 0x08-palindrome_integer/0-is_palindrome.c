648839ec7d8ccd986e7dad832d4c76bc1bea8602
jeanpierreba <2713@holbertonschool.com>
2022-02-09 13:13:22 -0500

    Added all files

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "palindrome.h"
+
+/**
+* is_palindrome - checks whether or not a given integer is a palindrome
+* @n: number to check
+* Return: 1 if number is palindrome 0 otherwise
+*/
+int is_palindrome(unsigned long n)
+{
+	int number = n, residue, reversed = 0;
+
+	while (n != 0)
+	{
+		residue = n % 10;
+		reversed = reversed * 10 + residue;
+		printf("%d\n", reversed);
+		printf("%d\n", residue);
+		n = n / 10;
+	}
+	if (number == reversed)
+		return (1);
+	return (0);
+}
