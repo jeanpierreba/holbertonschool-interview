2e4e9bd42f26a141d585e4a2cabcf08651324cf7
jeanpierreba <2713@holbertonschool.com>
2022-02-17 11:01:22 -0500

    Added all files

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Module that contains validUTF8 method """
+
+
+def validUTF8(data):
+    """ determines if a given data set represents a valid UTF-8 encoding """
+    number_bytes = 0
+    for d in data:
+        binary_rep = format(d, '#010b')[-8:]
+        if number_bytes == 0:
+            for bit in binary_rep:
+                if bit == '0':
+                    break
+                number_bytes += 1
+            if number_bytes == 0:
+                continue
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+        else:
+            if not (binary_rep[0] == '1' and binary_rep[1] == '0'):
+                return False
+        number_bytes -= 1
+    if not number_bytes:
+        return True
+    else:
+        return False
