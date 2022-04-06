fd52e84f9dbd0be7bbeddb9a5445024e74c7f983
jeanpierreba <2713@holbertonschool.com>
2022-04-06 09:28:17 -0500

    Added all files

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Prints all characters of a specific movie of star wars
+const request = require('request');
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (error, response, body) => {
+  if (!error) {
+    const character = JSON.parse(body).character;
+    requestPrint(character, 0);
+  }
+});
+
+function requestPrint (url, n) {
+  request(url[n], (error, response, body) => {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (n + 1 < url.length) requestPrint(url, ++n);
+    }
+  });
+}
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,1 @@
+# JavaScript starwars API
