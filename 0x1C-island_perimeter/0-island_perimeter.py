15663d5675f91d607b77b9b8e6b369b4a3663dc1
jeanpierreba <2713@holbertonschool.com>
2022-07-13 10:28:26 -0500

    Added all files

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Contains the function island_perimeter """
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island described in grid """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+    return perimeter
